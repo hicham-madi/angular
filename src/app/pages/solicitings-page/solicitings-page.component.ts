4e6ef4b9d37d59866f247627ee2b0c08046b6d2e
Hicham MADI <hicham.madi@gmail.com>
2025-05-14 17:07:41 +0200

    update

--- src/app/pages/solicitings-page/solicitings-page.component.ts
+++ src/app/pages/solicitings-page/solicitings-page.component.ts
@@ -1,16 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+// Feature Components
 import { TopBarComponent } from '../../components/top-bar/top-bar.component';
 import { PageHeaderComponent } from '../../components/page-header/page-header.component';
 import { CounterCardsComponent } from '../../components/counter-cards/counter-cards.component';
 import { FilterBarComponent } from '../../components/filter-bar/filter-bar.component';
-import { SolicitationsTableComponent } from '../../components/solicitations-table/solicitations-table.component';
 import { StatusTabsComponent } from '../../components/status-tabs/status-tabs.component';
-
+import { SolicitationsTableComponent } from '../../components/solicitations-table/solicitations-table.component';
 
 @Component({
   standalone: true,
-  selector: 'app-solicitation-page',
+  selector: 'app-solicitings-page',
   templateUrl: './solicitings-page.component.html',
   styleUrls: ['./solicitings-page.component.scss'],
   imports: [
@@ -21,6 +22,6 @@ import { StatusTabsComponent } from '../../components/status-tabs/status-tabs.co
     FilterBarComponent,
     StatusTabsComponent,
     SolicitationsTableComponent
-  ],
+  ]
 })
 export class SolicitingsPageComponent {}
