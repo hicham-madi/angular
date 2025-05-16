a25baa5c6ef42ddc84e3b73f9c0e14bf4e5e5a16
Hicham MADI <hicham.madi@gmail.com>
2025-05-16 20:22:26 +0200

    update

--- src/app/components/status-tabs/status-tabs.component.ts
+++ src/app/components/status-tabs/status-tabs.component.ts
@@ -1,13 +1,18 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-status-tabs',
-  standalone: true,
-  imports: [CommonModule],
   templateUrl: './status-tabs.component.html',
-  styleUrls: ['./status-tabs.component.scss']
+  styleUrls: ['./status-tabs.component.scss'],
+  standalone: true,
+  imports: [CommonModule]
 })
 export class StatusTabsComponent {
-  selected: 'ongoing' | 'completed' = 'ongoing';
+  @Input() tabs = [
+    { label: 'En cours', value: 'ongoing', count: 66 },
+    { label: 'Terminées', value: 'completed', count: 0 },
+  ];
+
+  selected = this.tabs[0].value;
 }
