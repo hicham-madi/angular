a50075e9e599cc48f588e060518d370233699ac5
Hicham MADI <hicham.madi@gmail.com>
2025-05-16 16:48:23 +0200

    update

--- src/app/components/dashboard-summary/dashboard-summary.component.ts
+++ src/app/components/dashboard-summary/dashboard-summary.component.ts
@@ -8,82 +8,73 @@ import { DashboardCardData } from '../../models/dashboard-card.model';
   standalone: true,
   templateUrl: './dashboard-summary.component.html',
   styleUrls: ['./dashboard-summary.component.scss'],
-  imports: [CommonModule, DashboardCardComponent]
+  imports: [CommonModule, DashboardCardComponent],
 })
 export class DashboardSummaryComponent {
-  cards: DashboardCardData[] = [
+  smallCards: DashboardCardData[] = [
     {
       icon: '../../assets/unread.svg',
       count: 4,
       label: 'non lues\nau total',
       small: true,
-      wide: false,
-      variant: 'small-red'
+      variant: 'small-red',
     },
     {
       icon: '../../assets/late.svg',
       count: 4,
       label: 'en retard',
       small: true,
-      wide: false,
-      variant: 'small-red'
+      variant: 'small-red',
     },
     {
       icon: '../../assets/affect.svg',
       count: 4,
       label: 'affecter\nà l\'équipe',
       small: true,
-      wide: false,
-      variant: 'small-red'
+      variant: 'small-red',
     },
+  ];
+
+  largeCards: DashboardCardData[] = [
     {
       icon: '../../assets/request.svg',
       count: 28,
       label: 'demandes',
+      variant: 'yellow',
       badges: [{ icon: '../../assets/unread.svg', value: 2 }],
-      small: false,
-      wide: false,
-      variant: 'yellow'
     },
     {
       icon: '../../assets/claim.svg',
       count: 11,
       label: 'réclamations',
+      variant: 'red',
       badges: [{ icon: '../../assets/unread.svg', value: 0 }],
-      small: false,
-      wide: false,
-      variant: 'red'
     },
     {
       icon: '../../assets/approval.svg',
       count: 13,
       label: 'approbations',
+      variant: 'purple',
       badges: [{ icon: '../../assets/unread.svg', value: 2 }],
-      small: false,
-      wide: false,
-      variant: 'purple'
     },
     {
       icon: '../../assets/discussion-group.svg',
       count: 28,
       label: 'groupes de discussion',
+      variant: 'blue',
       badges: [{ icon: '../../assets/unread.svg', value: 0 }],
-      small: false,
-      wide: false,
-      variant: 'blue'
     },
     {
       icon: '../../assets/request-expert-service.svg',
       count: 28,
       label: 'demandes services expert',
-      wide: true,
-      small: false,
       variant: 'turquoise',
+      wide: true,
       badges: [
+        { icon: '../../assets/unread.svg', value: 2 },
+        { icon: '../../assets/unread.svg', value: 1 },
         { icon: '../../assets/unread.svg', value: 0 },
-        { icon: '../../assets/affect.svg', value: 0 },
-        { icon: '../../assets/late.svg', value: 0 }
-      ]
-    }
+      ],
+    },
   ];
 }
